d8d2eee86f80f1aedb1a29120acd1b8cab37c51e
Sanchit475 <sanchit.yadav19april@gmail.com>
2026-03-10 14:37:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+const balance = document.getElementById('balance')
+const money_plus = document.getElementById('money-plus')
+const money_minus = document.getElementById('money-minus')
+const list = document.getElementById('list')
+const form = document.getElementById('form')
+const text = document.getElementById('text')
+const amount = document.getElementById('amount')
+
+let transactions = []
+
+function addTransaction(e){
+e.preventDefault()
+
+const transaction = {
+id: Date.now(),
+text: text.value,
+amount: +amount.value
+}
+
+transactions.push(transaction)
+
+addTransactionDOM(transaction)
+updateValues()
+
+text.value=''
+amount.value=''
+}
+
+function addTransactionDOM(transaction){
+
+const sign = transaction.amount < 0 ? '-' : '+'
+
+const item = document.createElement('li')
+
+item.innerHTML =
+`${transaction.text} <span>${sign}₹${Math.abs(transaction.amount)}</span>`
+
+list.appendChild(item)
+}
+
+function updateValues(){
+
+const amounts = transactions.map(t => t.amount)
+
+const total = amounts.reduce((acc,item)=>acc+item,0)
+
+const income = amounts
+.filter(item=>item>0)
+.reduce((acc,item)=>acc+item,0)
+
+const expense = amounts
+.filter(item=>item<0)
+.reduce((acc,item)=>acc+item,0)
+
+balance.innerText = `₹${total}`
+money_plus.innerText = `₹${income}`
+money_minus.innerText = `₹${Math.abs(expense)}`
+}
+
+form.addEventListener('submit',addTransaction)
